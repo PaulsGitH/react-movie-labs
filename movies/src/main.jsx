653ea7731786411382190cd253f634f615df2bda
Paul <20096590@mail.wit.ie>
2025-10-13 21:16:25 +0100

    Added new review support.

--- movies/src/main.jsx
+++ movies/src/main.jsx
@@ -1,13 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { BrowserRouter, Route, Navigate, Routes } from "react-router";
-import HomePage from "./pages/homePage";
-import MoviePage from "./pages/movieDetailsPage";
-import FavoriteMoviesPage from "./pages/favoriteMoviesPage.jsx";
-import MovieReviewPage from "./pages/movieReviewPage";
-import SiteHeader from "./components/siteHeader";
 import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import SiteHeader from "./components/siteHeader";
+import HomePage from "./pages/homePage";
+import MoviePage from "./pages/movieDetailsPage";
+import FavoriteMoviesPage from "./pages/favoriteMoviesPage";
+import MovieReviewPage from "./pages/movieReviewPage";
+import AddMovieReviewPage from "./pages/addMovieReviewPage";
 import MoviesContextProvider from "./contexts/moviesContext";
 
 const queryClient = new QueryClient({
@@ -27,8 +28,9 @@ const App = () => {
         <SiteHeader />
         <MoviesContextProvider>
           <Routes>
-            <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
+            <Route path="/reviews/form" element={<AddMovieReviewPage />} />
             <Route path="/reviews/:id" element={<MovieReviewPage />} />
+            <Route path="/movies/favorites" element={<FavoriteMoviesPage />} />
             <Route path="/movies/:id" element={<MoviePage />} />
             <Route path="/" element={<HomePage />} />
             <Route path="*" element={<Navigate to="/" />} />
@@ -40,5 +42,5 @@ const App = () => {
   );
 };
 
-const rootElement = createRoot(document.getElementById("root"));
-rootElement.render(<App />);
+const root = createRoot(document.getElementById("root"));
+root.render(<App />);
